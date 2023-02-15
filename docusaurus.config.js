7e9af7e769910363a605f07c37a9c1f6ad600f68
yangsong13 <yangsong13@meituan.com>
2023-02-15 23:53:18 +0800

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,33 +25,33 @@ const config = {
     require.resolve("./plugins/optimize/index"),
     require.resolve("./plugins/manifest/index"),
     require.resolve("./plugins/delay-code-block-appearance"),
-    [
-      "@docusaurus/plugin-pwa",
-      {
-        pwaHead: [
-          {
-            tagName: "link",
-            rel: "manifest",
-            href: "/manifest.webmanifest",
-          },
-          {
-            tagName: "meta",
-            name: "theme-color",
-            content: "#21222c",
-          },
-          {
-            tagName: "meta",
-            name: "apple-mobile-web-app-capable",
-            content: "yes",
-          },
-          {
-            tagName: "meta",
-            name: "apple-mobile-web-app-status-bar-style",
-            content: "#21222c",
-          },
-        ],
-      },
-    ],
+    // [
+    //   "@docusaurus/plugin-pwa",
+    //   {
+    //     pwaHead: [
+    //       {
+    //         tagName: "link",
+    //         rel: "manifest",
+    //         href: "/manifest.webmanifest",
+    //       },
+    //       {
+    //         tagName: "meta",
+    //         name: "theme-color",
+    //         content: "#21222c",
+    //       },
+    //       {
+    //         tagName: "meta",
+    //         name: "apple-mobile-web-app-capable",
+    //         content: "yes",
+    //       },
+    //       {
+    //         tagName: "meta",
+    //         name: "apple-mobile-web-app-status-bar-style",
+    //         content: "#21222c",
+    //       },
+    //     ],
+    //   },
+    // ],
     [
       require.resolve("./plugins/blog"),
       {
